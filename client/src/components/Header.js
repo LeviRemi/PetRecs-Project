101598e22403329cb251396e4bf0a5ee78c0732b
6erik <estanton@csumb.edu>
2020-09-04 01:42:16 -0700

    Added react-bootstrap.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,16 @@
 // Header.js
 
 import React from 'react'
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
 function Header() {
     return (
         <div>
             <h1>PetRecs</h1>
+            <Breadcrumb>
+                <Breadcrumb.Item as="h5">Pets</Breadcrumb.Item>
+                <Breadcrumb.Item as="h2" href="#">My Account</Breadcrumb.Item>
+            </Breadcrumb>
         </div>
     )
 }
