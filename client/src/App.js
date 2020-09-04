965f9d77ae54d8ebf1a5a68847ab62fe145b536b
6erik <estanton@csumb.edu>
2020-09-04 00:47:04 -0700

    Started building template. Header and footer components added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
+function App(){
+
+  const sayHello = () => {
+    console.log('hello');
+  }
+
+  const counter = 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+
+        -- Main app/body placeholder --<br />
+        -- Main app/body placeholder --<br />
+        -- Main app/body placeholder --<br />
+        -- Main app/body placeholder --<br />
+
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+// Footer.js
+
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <h5>
+              PetRecs - Footer info here <br />
+              2nd line of Footer info here <br />
+            </h5>
+        </div>
+    )
+}
+
+export default Footer
