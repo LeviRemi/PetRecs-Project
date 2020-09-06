1f6d19776823641c0af2b2dd4335c6debae87182
John Fleischli <jcfleischli@gmail.com>
2020-09-05 21:36:01 -0700

    adds pet div template on Pets page

--- client/src/components/Pets.js
+++ client/src/components/Pets.js
@@ -7,11 +7,58 @@ import Header from './Header.js';
 import Footer from './Footer.js';
 
 function Pets() {
+
+    const petContainerStyle = {
+        width: 135,
+        height: 170,
+        marginTop: 25,
+        marginBottom: 25,
+        marginLeft: 12.5,
+        marginRight: 12.5
+    };
+
+    const circleStyle = {
+            width: 135,
+            height: 135,
+            borderRadius: 135/2,
+            borderStyle: "solid",
+            borderColor: "gray"
+    };
+
+    const nameStyle = {
+        textAlign: "center",
+        fontWeight: 700,
+        fontSize: 23
+    };
+
+    const addTextStyle = {
+        textAlign: "center",
+        fontWeight: 700,
+        fontSize: 23,
+        paddingTop: 45,
+        color: "gray"
+    };
+
+    const linkStyle = {
+        textDecoration: "none",
+        color: "gray"
+    };
+
     return (
         
         <div>
             <Header />
-            <p>This is the Pets Page.</p>
+            <Link style={linkStyle} to="/Pets">
+            <div style={petContainerStyle}>
+                <div style={circleStyle}>
+                    <p style={addTextStyle}>Add Pet</p>
+                </div>
+                <div>
+                    <p style={nameStyle}>Pet Name</p>
+                </div>
+            </div>
+            </Link>
+            
             <br/><br/>
             
             <Footer />
