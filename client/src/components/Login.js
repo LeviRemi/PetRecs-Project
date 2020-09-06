ea13e907e68765d8731f7ef5e538d3a3e45f313d
6erik <estanton@csumb.edu>
2020-09-06 13:59:49 -0700

    Updated login and registration component styles

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,52 @@
 // Login.js
 
 import React from 'react'
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+
+function Login() {
+    return (
+        <div className="fill-window">
+            
+            <div className="formBox">
+
+                <div className="formTitle">
+                    LOGIN
+                </div>
+                <br />
+                <Form className="loginRegForm">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>EMAIL ADDRESS</Form.Label>
+                    <Form.Control type="email" placeholder="example@mail.com" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>PASSWORD</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">TEST FORM LOGIN BUTTON -- log in</Button>
+                </Form>
+                
+                <br /><br />
+                <Link to="/Pets" className="btn btn-secondary">Login</Link>
+            </div>
+
+
+            
+        </div>
+    )
+}
+
+export default Login
+
+
+
+/*
+
+import React from 'react'
 import { Link } from 'react-router-dom';
 
 function Login() {
@@ -11,9 +57,12 @@ function Login() {
                 <h1>Login</h1>
             </div>
             <p>This is the Login Page.</p>
+            
             <Link to="/Pets" className="btn btn-secondary">Login</Link>
         </div>
     )
 }
 
 export default Login
+
+*/
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,17 +1,46 @@
 // Register.js
 
 import React from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import { Link } from 'react-router-dom';
 
 function Register() {
     return (
-        
-        <div>
-            <div>
-                <h1>Register</h1>
+        <div className="fill-window">
+
+        <div className="formBox">
+
+        <div className="formTitle">
+            REGISTER
+        </div>
+        <br />
+                <Form className="loginRegForm">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>EMAIL ADDRESS</Form.Label>
+                    <Form.Control type="email" placeholder="example@mail.com" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>PASSWORD</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicRepeatPassword">
+                    <Form.Label>REPEAT PASSWORD</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">TEST FORM REGISTER BUTTON -- Register</Button>
+                </Form>
+                
+                <br /><br />
+                <Link to="/" className="btn btn-secondary">Register</Link>
             </div>
-            <p>This is the Register Page.</p>
-            <Link to="/" className="btn btn-secondary">Register</Link>
+
+
+            
         </div>
     )
 }
