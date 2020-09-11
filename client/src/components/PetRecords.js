9a7c20bdb0285b8bd60d9f258f309a1685bf5e0e
6erik <estanton@csumb.edu>
2020-09-10 18:31:05 -0700

    Added post-login styling to PetRecords

--- client/src/components/PetRecords.js
+++ client/src/components/PetRecords.js
@@ -6,6 +6,9 @@ import { Link } from 'react-router-dom';
 import Header from './Header.js';
 import Footer from './Footer.js';
 
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
 import DropdownButton from 'react-bootstrap/DropdownButton';
@@ -13,19 +16,44 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 function PetRecords() {
     return (
-      <div>
+      <div className="fullPageContainer">
+        
+        <div>
           <Header />
+        </div>
+        <div className="mainPageBody nopadding">
+          <Container fluid>
+            <Row>
+              <Col xs="2" className="nopadding">
+                <div className="mainPageSideBtnGroup">
+                  <button className="btn-mainpage-side">
+                    Records
+                  </button><br />
+                  <button className="btn-mainpage-side">
+                    Health
+                  </button><br />
+                  <button className="btn-mainpage-side">
+                    Event
+                  </button><br />
+                  <button className="btn-mainpage-side">
+                    Reminders
+                  </button>
+                  
+                </div>
+              </Col>
 
-          <ButtonGroup vertical>
-            <Button>Records</Button>
-            <Button>Health</Button>
-            <Button>Events</Button>
-            <Button>Reminders</Button>
-          </ButtonGroup>
-
-          <h3> need to figure out here whether we will use multiple pages<br />
-          or if this should be a component that renders in when needed</h3>
+              <Col lg={true} className="nopadding">
+                <div className="mainPageContents">
+                  <h3> need to figure out here whether we will use multiple pages<br />
+                  or if this should be a component that renders in when needed</h3>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="mainPageFooter">
           <Footer />
+        </div> 
       </div>
     )
 }
