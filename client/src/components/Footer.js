8aa9b399812fcc80aa47e7d47fbe296b4e724884
LeviRemi <leviremi@gmail.com>
2020-09-19 01:58:35 -0700

    Added default copyright to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,13 +4,13 @@ import React from 'react'
 
 function Footer() {
     return (
-        <div>
+        <div style={{textAlign: "center"}}>
             <hr />
-            <h5>
-              PetRecs - Footer info here <br />
-              2nd line of Footer info here <br />
-            </h5>
-        </div>
+            <small>Copyright &copy; {new Date().getFullYear()} PetRecs. All Rights Reserved<br /></small>
+            {// if we use the pet icons...
+             //<small>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></small>
+            }
+            </div>
     )
 }
 
