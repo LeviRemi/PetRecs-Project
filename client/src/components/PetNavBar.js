ca0b7de4e16e6d98664144695d68f2e7fc7dde60
6erik <estanton@csumb.edu>
2020-09-21 01:05:09 -0700

    Adding pet weight uses PetId from prop. Still needs current date/time

--- client/src/components/PetNavBar.js
+++ client/src/components/PetNavBar.js
@@ -12,32 +12,34 @@ function PetNavBar(props) {
       
         <div className="petProfileNavBar">
               <Row>
-              <Col sm="1">
-              </Col>
-              <Col>
-                  <NavLink to={{pathname: `/Pets/${props.value.PetId}/Records`}}>  
-                    <button className="btn-petprofile-nav">
-                      Records
-                    </button>
-                  </NavLink>
-                  <NavLink to={{pathname: `/Pets/${props.value.PetId}/Health`}}>
-                    <button className="btn-petprofile-nav">
-                      Health
-                    </button>
-                  </NavLink>
-                  <NavLink to={{pathname: `/Pets/${props.value.PetId}/Events`}}>
-                    <button className="btn-petprofile-nav">
-                      Events
-                    </button>
-                  </NavLink>
-                  <NavLink to={{pathname: `/Pets/${props.value.PetId}/Reminders`}}>
-                    <button className="btn-petprofile-nav">
-                      Reminders
-                    </button>
-                  </NavLink>
-              </Col>
-              <Col sm="1">
-              </Col>
+                <Col sm="1">
+                </Col>
+
+                <Col>
+                    <NavLink to={{pathname: `/Pets/${props.value.PetId}/Records`}}>  
+                      <button className="btn-petprofile-nav">
+                        Records
+                      </button>
+                    </NavLink>
+                    <NavLink to={{pathname: `/Pets/${props.value.PetId}/Health`}}>
+                      <button className="btn-petprofile-nav">
+                        Health
+                      </button>
+                    </NavLink>
+                    <NavLink to={{pathname: `/Pets/${props.value.PetId}/Events`}}>
+                      <button className="btn-petprofile-nav">
+                        Events
+                      </button>
+                    </NavLink>
+                    <NavLink to={{pathname: `/Pets/${props.value.PetId}/Reminders`}}>
+                      <button className="btn-petprofile-nav">
+                        Reminders
+                      </button>
+                    </NavLink>
+                </Col>
+
+                <Col sm="1">
+                </Col>
               </Row>
           </div>
     )
