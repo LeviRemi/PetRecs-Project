92836ed532f3f2da02f9dead6ba0e7fbea94e1c8
Levi Remington <Leviremi@gmail.com>
2020-09-29 10:39:45 -0700

    Update authenticate.js
    Adding path omissions for frontend routes (homepage, login, register, about)

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,7 +3,7 @@ function authenticate(req, res, next) {
     console.log(req.path);
     // omit login and register and session API routes
     if (req.path === '/api/accounts/login/' || req.path === '/api/accounts/' || req.path === '/api/sessions/destroy'
-    || req.path === '/api/accounts/login' || req.path === '/api/accounts') {
+    || req.path === '/api/accounts/login' || req.path === '/api/accounts' || req.path === '/' || req.path === '/login' || req.path === '/register' || req.path === '/about') {
         return next();
     }
     if (!req.session || !req.session.user) {
