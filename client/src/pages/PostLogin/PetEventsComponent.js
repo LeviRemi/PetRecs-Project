e66e338a06b0072d0213b0ecbc52b0aa083f1805
6erik <estanton@csumb.edu>
2020-09-28 19:00:45 -0700

    Events update form working. DefaultValue for updateform not working

--- client/src/pages/PostLogin/PetEventsComponent.js
+++ client/src/pages/PostLogin/PetEventsComponent.js
@@ -18,19 +18,35 @@ export default class PetEventsComponent extends Component {
     super();
     this.state = { PetId: props.match.params.PetId,
                    events: [],
-                   show: false };
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+                   EventId: '',
+                   showAdd: false,
+                   showUpdate: false };
+    this.handleShowAdd = this.handleShowAdd.bind(this);
+    this.handleCloseAdd = this.handleCloseAdd.bind(this);
+    this.handleShowUpdate = this.handleShowUpdate.bind(this);
+    this.handleCloseUpdate = this.handleCloseUpdate.bind(this);
 
     console.log("PetEventsComponent - Using PetId: " + this.state.PetId);
   }
 
-	handleClose() {
-		this.setState({ show: false });
+	handleCloseAdd() {
+		this.setState({ showAdd: false });
 	}
 
-	handleShow() {
-		this.setState({ show: true });
+	handleShowAdd() {
+		this.setState({ showAdd: true });
+  }
+
+  handleCloseUpdate() {
+		this.setState({ showUpdate: false });
+	}
+
+	handleShowUpdate() {
+    this.setState({ showUpdate: true });
+  }
+
+  updateStateEventId(buttonEventId) {
+    this.setState({ EventId: buttonEventId });
   }
   
   deleteEvent = async (EventId) => {
@@ -57,8 +73,6 @@ export default class PetEventsComponent extends Component {
       })
   };
 
-  
-
   eventTypeIdToString(TypeId) {
     let stringType = "";
     if (TypeId === 1) { stringType = "Medical"; } 
@@ -80,7 +94,14 @@ export default class PetEventsComponent extends Component {
           <td>{this.eventTypeIdToString(EventTypeId)}</td>
           <td>{moment(Date).format("MM/DD/YYYY")}</td>
           <td>{EventDescription}</td>
-          <td><Button size="sm" variant="info">&#x270E;</Button></td>
+          <td>
+            <Button size="sm" variant="info" onClick={() => {
+                this.updateStateEventId(EventId);
+                this.handleShowUpdate();
+             }}>
+                &#x270E;
+            </Button>
+            </td>
           <td><Button size="sm" variant="danger" onClick={ () => { this.deleteEvent(EventId)}}>&#x2716;</Button></td>
         </tr>
       )
@@ -108,10 +129,10 @@ export default class PetEventsComponent extends Component {
           </Table>
         </div>
         <div>
-          <Button onClick={this.handleShow} variant="outline-dark">Add Event</Button>
+          <Button onClick={this.handleShowAdd} variant="outline-dark">Add Event</Button>
           <Modal
-                show={this.state.show}
-                onHide={this.handleClose}
+                show={this.state.showAdd}
+                onHide={this.handleCloseAdd}
                 backdrop="static"
                 keyboard={false}
             >
@@ -122,8 +143,26 @@ export default class PetEventsComponent extends Component {
                 <AddEventComponent petid={this.state.PetId}/>
             </Modal.Body>
             <Modal.Footer>
-                    <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+                    <Button variant="secondary" onClick={this.handleCloseAdd}>Close</Button>
                     <Button variant="primary" type="submit" form="AddEventForm">Add Event</Button>
+            </Modal.Footer>
+          </Modal>
+
+          <Modal
+                show={this.state.showUpdate}
+                onHide={this.handleCloseUpdate}
+                backdrop="static"
+                keyboard={false}
+            >
+            <Modal.Header closeButton>
+            <Modal.Title>Update Event</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <UpdateEventComponent eventid={this.state.EventId}/>
+            </Modal.Body>
+            <Modal.Footer>
+                    <Button variant="secondary" onClick={this.handleCloseUpdate}>Close</Button>
+                    <Button variant="primary" type="submit" form="UpdateEventForm">Update Event</Button>
             </Modal.Footer>
           </Modal>
         </div>
@@ -211,6 +250,112 @@ class AddEventComponent extends Component {
                     <Form.Label>Description</Form.Label>
                     <Form.Control name="eventDescription" type="textarea" as="textarea" rows="5"
                                   placeholder="Enter a description of the event here..."
+                                  onChange={this.handleEventDescriptionChange}
+                                  required/>
+                </Form.Group>
+              </Col>
+            </Form.Row>
+          </Form>
+      </div>
+    )
+  }
+}
+
+
+class UpdateEventComponent extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      EventTypeId: "",
+      EventId: props.eventid,
+      EventDescription: "",
+      Date: ''}
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:5000/api/pet-events/` + this.state.EventId, {withCredentials: true} )
+      .then(response=>{
+        this.setState({EventTypeId: response.data.EventTypeId,
+                        EventId: response.data.EventId,
+                        EventDescription: response.data.EventDescription,
+                        Date: response.data.Date});
+      })
+      .catch((error) => {
+          console.log(error);
+      })
+  }
+
+  handleEventTypeIdChange = event => {
+    this.setState({EventTypeId: event.target.value});
+  }
+
+  handleEventDescriptionChange = event => {
+    this.setState({EventDescription: event.target.value});
+  }
+
+  handleDateChange = event => {
+    this.setState({Date: event.target.value});
+  }
+
+  handleUpdate = event => {
+    event.preventDefault();
+
+  const data = {
+      EventTypeId: this.state.EventTypeId,
+      PetId: this.state.PetId,
+      EventDescription: this.state.EventDescription,
+      Date: this.state.Date,
+    };
+
+  axios.put(`http://localhost:5000/api/pet-events/` + this.state.EventId, data, {withCredentials: true} )
+        .then(response=>{
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+  }
+
+  render() {
+    return (
+      <div className="formBoxAddEvent">
+          <Form id="UpdateEventForm" onSubmit={this.handleUpdate}>
+            <Form.Row>
+              <Col>
+                <Form.Group controlId="formEventType">
+                <Form.Label>Event Type</Form.Label>
+                <Form.Control name="eventTypeId" as="select"
+                              onChange={this.handleEventTypeIdChange}
+                              defaultValue={{
+                                value: this.state.EventTypeId
+                              }} >
+                  <option value="1">Medical</option>
+                  <option value="2">Grooming</option>
+                  <option value="3">Fitness</option>
+                  <option value="4">Food</option>
+                  <option value="5">Potty</option>
+                  <option value="6">Behavior</option>
+                  <option value="7">Other</option>
+                </Form.Control>
+                </Form.Group>
+              </Col>
+
+              <Col>
+                <Form.Group controlId="formDate">
+                <Form.Label>Date</Form.Label>
+                <Form.Control name="date" type="date" max={moment().format("YYYY-MM-DD")}
+                              onChange={this.handleDateChange}
+                              required/>
+                </Form.Group>
+              </Col>
+            </Form.Row>
+                
+            <Form.Row>
+              <Col>
+                <Form.Group controlId="formEventDescription">
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control name="eventDescription" type="textarea" rows={5}
+                                  defaultValue={this.state.EventDescription}
                                   onChange={this.handleEventDescriptionChange}
                                   required/>
                 </Form.Group>
