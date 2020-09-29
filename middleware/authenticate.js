0ca1ba2c73113078d982ab6914fb6904a579026a
Levi Remington <Leviremi@gmail.com>
2020-09-29 11:53:02 -0700

    Update authenticate.js
    Added omissions for static folder, manifest, favicon, and logo

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -3,7 +3,8 @@ function authenticate(req, res, next) {
     console.log(req.path);
     // omit login and register and session API routes
     if (req.path === '/api/accounts/login/' || req.path === '/api/accounts/' || req.path === '/api/sessions/destroy'
-    || req.path === '/api/accounts/login' || req.path === '/api/accounts' || req.path === '/' || req.path === '/login' || req.path === '/register' || req.path === '/about') {
+    || req.path === '/api/accounts/login' || req.path === '/api/accounts' || req.path === '/' || req.path === '/login' || req.path === '/register' || req.path === '/about'
+       || req.path === '/static*' || req.path === '/manifest.json' || req.path === '/favicon.ico' || req.path === '/logo192.png') {
         return next();
     }
     if (!req.session || !req.session.user) {
