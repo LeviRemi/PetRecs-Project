3907ebfd2181949d19045028c43d6962aa1653af
Levi Remington <Leviremi@gmail.com>
2020-09-29 12:40:33 -0700

    Update Logout.js
    Forgot to add useHistory

--- client/src/utils/Logout.js
+++ client/src/utils/Logout.js
@@ -5,6 +5,8 @@ import React from "react";
 import HomePage from "../pages/HomePage";
 
 function Logout() {
+    
+    const history = useHistory();
 
     // Destroy session
     axios.get("/api/sessions/destroy", { withCredentials: true })
