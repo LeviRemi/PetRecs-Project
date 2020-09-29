f93b4266d32959b44d0bbd197da9b85b39262d0e
John Fleischli <jcfleischli@gmail.com>
2020-09-29 13:57:11 -0700

    Update authenticate.js
    Removes debugging logs

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -9,8 +9,6 @@ function authenticate(req, res, next) {
     }
     if (!req.session || !req.session.user) {
         const err = new Error("You shall not pass");
-        console.log("Session: "+req.session);
-        console.log("User: "+req.session.user);
         err.statusCode = 401;
         next(err);
     }
