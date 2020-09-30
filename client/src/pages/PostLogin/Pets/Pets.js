765ba4203211392b70103585810a4f00de0e063e
6erik <estanton@csumb.edu>
2020-09-30 01:14:34 -0700

    Updated Pets.js to match PetScene style.

--- client/src/pages/PostLogin/Pets/Pets.js
+++ client/src/pages/PostLogin/Pets/Pets.js
@@ -37,36 +37,43 @@ export default class Pets extends Component {
     render() {
     const account = this.state.account;
     return (
-        <div className="fullPageContainer">
-          <div>
-            <Header />
-          </div>
-          <Container fluid>
-            <div className="mainPageBody nopadding">
-                <div className="mainPageContents">
-                  { this.state.pets.map(pet => 
-                  <Link id="PetLink" to={"/Pets/" + pet.PetId} key={pet.PetId}>
-                    <PetImage {...{PetId: pet.PetId, ProfileUrl: pet.ProfileUrl}}/>
-                      <div>
-                          <p className="NameText">{pet.PetName}</p> 
-                      </div>
-                  </Link>
-                  )}
-                  <Link id="PetLink" to={"/Pets/New"}>
-                    <div id="AddPetContainer">
-                      <div id="AddPetCircle">
-                        <img className="AddPetImage"></img>
-                        <p id="AddPetText">Add Pet<br />< FontAwesomeIcon id="PlusIcon" icon="plus" size="2x" /></p>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-            </div>
-          </Container>
-          <div className="mainPageFooter">
-            <Footer />
-          </div> 
+
+      <div className="fontWrap">
+        <div>
+          <Header />
         </div>
+      
+        <div className="fullPageContainer">
+          <div className="mainContent">
+            <Container fluid>
+              <div className="mainPageBody nopadding">
+                  <div className="mainPageContents">
+                    { this.state.pets.map(pet => 
+                    <Link id="PetLink" to={"/Pets/" + pet.PetId} key={pet.PetId}>
+                      <PetImage {...{PetId: pet.PetId, ProfileUrl: pet.ProfileUrl}}/>
+                        <div>
+                            <p className="NameText">{pet.PetName}</p> 
+                        </div>
+                    </Link>
+                    )}
+                    <Link id="PetLink" to={"/Pets/New"}>
+                      <div id="AddPetContainer">
+                        <div id="AddPetCircle">
+                          <img className="AddPetImage"></img>
+                          <p id="AddPetText">Add Pet<br />< FontAwesomeIcon id="PlusIcon" icon="plus" size="2x" /></p>
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
+              </div>
+            </Container>
+
+            <div className="mainPageFooter">
+              <Footer />
+            </div>
+          </div>
+      </div>
+    </div>   
     )
-    }
+  }
 }
