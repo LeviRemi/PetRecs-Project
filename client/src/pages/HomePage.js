19c0d06a65d89d48f0888058715d17349014bd75
LeviRemi <Leviremi@gmail.com>
2020-10-12 15:38:36 -0700

    (Upgrade) HomePage style improvements and text refinements.

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -17,6 +17,12 @@ function HomePage() {
         backgroundColor: 'var(--COLOR_WHITE)',
     };
 
+    const featureTitleImageStyle = {
+        display: "block",
+        margin: "auto",
+        marginBottom: "16px"
+    };
+
     const featureDescStyle = {
         fontSize: 15,
         textAlign: 'center',
@@ -24,78 +30,79 @@ function HomePage() {
     };
 
     return (
-        <div className="fullPageContainer fontWrap">
-            <div className="homePageHeader">
-                <div className="homePageHeaderItem">
-                    <img src={require('../icon_lg.png')} alt='PetRecs Dogument Logo' width='58' height='64' />
-                    PetRecs
+        <div className="fontWrap">
+            <div className="fullPageContainer FadeIn">
+                <div className="homePageHeader">
+                    <div className="homePageHeaderItem">
+                        <img src={require('../icon_lg.png')} alt='PetRecs Dogument Logo' width='58' height='64' />
+                        PetRecs
+                    </div>
+
+                    <div className="homePageHeaderItem push-right">
+                        <Link to="/register">
+                            <button className="btn-homepage-header btn-homepage-header-register">
+                                Create an Account
+                            </button>
+                        </Link>
+                        <Link to="/login" style={{marginLeft: "10px", marginRight: "10px"}}>
+                            <button className="btn-homepage-header btn-homepage-header-login">
+                                Log In
+                            </button>
+                        </Link>
+                    </div>
                 </div>
 
-                <div className="homePageHeaderItem push-right">
-                    <Link to="/register">
-                        <button className="btn-homepage-header btn-homepage-header-register">
-                            Create an Account
-                        </button>
-                    </Link>
-                    <Link to="/login">
-                        <button className="btn-homepage-header">
-                            Log In
-                        </button>
-                    </Link>
+                <div className="img-banner">
+                    <img src={require('../dog-bone-banner.jpg')} className="img-fluid" alt="Different dog breeds on a bone"></img>
                 </div>
-            </div>
 
-            <div className="img-banner">
-                <img src={require('../dog-bone-banner.jpg')} className="img-fluid" alt="Different dog breeds on a bone"></img>
-            </div>
-
-            <div className="homePageBody">
-                PetRecs is the place for you to store all of the information for your furry, fluffy, or scaly friend. Store medical records
-                digitally, track health data, and keep record of important events all in one place.
-                <Container>
-                    <Row>
-                        <Col>
-                            <p style={featureTitleStyle}>Create Individual Profiles<br /></p>
-                        </Col>
-                        <Col>
-                            <p style={featureTitleStyle}>Store Records Digitally<br /></p>
-                        </Col>
-                        <Col>
-                            <p style={featureTitleStyle}>Monitor Health Data<br /></p>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                        <p style={featureDescStyle}>
-                        Setup a Pet Profile for each of your pets. This
-                        allows you to keep all your information
-                        separate and only share the pets you want.
-                        </p>
-                        </Col>
-                        <Col>
-                        <p style={featureDescStyle}>
-                        Upload scans/digital copies of your medical records
-                        from the Vet. Minimize the chance of
-                        physical records getting misplaced or
-                        damaged.
-                        </p>
-                        </Col>
-                        <Col>
-                        <p style={featureDescStyle}>
-                        Track weight, current medication, and
-                        significant events related to health. Have it
-                        accessible all in once place to make sure you
-                        don't forget something important.
-                        </p>
-                        </Col>
-                    </Row>
+                <div className="homePageBody">
+                    <p style={{textAlign: "center"}}>PetRecs is a digital hub to track health records for all of your furry, scaly, and feathered friends.<br/>
+                    Register today and begin your journey towards better pet record management.</p>
+                    <Container>
+                        <Row>
+                            <Col>
+                                <p style={featureTitleStyle}>Create Pet Profiles</p>
+                                <img style={featureTitleImageStyle} src={require('../pet-house.png')} alt='Create Pet Profiles image' width='125' height='125' />
+                            </Col>
+                            <Col>
+                                <p style={featureTitleStyle}>Store Records Online<br /></p>
+                                <img style={featureTitleImageStyle} src={require('../file-icon.png')} alt='Create Pet Profiles image' width='125' height='125' />
+                            </Col>
+                            <Col>
+                                <p style={featureTitleStyle}>Monitor Health Data<br /></p>
+                                <img style={featureTitleImageStyle} src={require('../veterinary-icon.png')} alt='Create Pet Profiles image' width='125' height='125' />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                            <p style={featureDescStyle}>
+                            Setup and customize individual Pet Profiles.
+                            Keep each profile separate and private.
+                            Share selected Pet Profiles with family, friends, or vets.
+                            </p>
+                            </Col>
+                            <Col>
+                            <p style={featureDescStyle}>
+                            Upload digital copies of your pet's medical records.
+                            Add custom labels and notes for organization.
+                            Sort and search through stored records with ease.
+                            </p>
+                            </Col>
+                            <Col>
+                            <p style={featureDescStyle}>
+                            Track weights, manage current medications, and
+                            log health events all in one place. Optimize
+                            pet care by keeping better track of your pet's health.
+                            </p>
+                            </Col>
+                        </Row>
                     </Container>
+                </div>
             </div>
-
             <div>
                 <Footer />
             </div>
-
         </div>
     )
 }
